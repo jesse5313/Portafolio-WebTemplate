7f4dab63950911df55afda5cf8ac7decafd2ccb4
Jesse Salomón <74323575+jesse5313@users.noreply.github.com>
2023-07-27 18:01:46 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+﻿jQuery(function($){
+
+'use strict';
+/* Preloader */
+(function () {
+$(window).load(function() {
+    $('#pre-status').fadeOut();
+    $('#st-preloader').delay(350).fadeOut('slow');
+});
+}());
+
+/* Animated scrolling / Scroll Up */
+(function () {
+$('a[href*=#]').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+});
+}());
+
+/* Full Screen */
+(function () {
+$(".st-fullHeight").height($(window).height());
+$(window).resize(function(){
+    $(".st-fullHeight").height($(window).height());
+});
+}());
+
+/* Parallax */
+(function () {
+$(window).bind('load', function () {
+    parallaxInit();						  
+});
+function parallaxInit() {		
+$(".subscribe").parallax("50%", 0.3);
+}	
+parallaxInit();
+}());
+});
